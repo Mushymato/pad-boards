6e898ea8f71ba0e77bdb825279e8b209b1ae0b38
xiaxinti <michelle.xia@mail.utoronto.ca>
2019-01-07 20:18:14 -0500

    css changes

--- display_boards.php
+++ display_boards.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 <link rel="stylesheet" type="text/css" href="boards.css">
+<link rel="stylesheet" type="text/css" href="style.css">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" type="text/javascript"></script>
 <!--<script src="jquery.min.js" type="text/javascript"></script>-->
 <script src="board_filters.js" type="text/javascript"></script>
@@ -17,12 +18,25 @@ $(document).ready(function(){
 </script>
 </head>
 <body>
+    <div class="header">
+        <div class="header-bg">
+        </div>
+        <div class="content">
+            <span class="pad-title">Puzzle & Dragons</span>
+            <p class="heading-title"><a href="./">Boards Database</a></p>
+            <ul class="navbar">
+            	<li><a href="#">About</a></li>
+                <li><a href="../">Return to Blog</a></li>
+            </ul>
+        </div>
+    </div>
+    <section id="display-boards">
 <?php
 function option($value, $text, $check){
 	return '<option value="'. $value .'"' . ($check == $value ? ' selected' : ' ') . '>' . $text . '</option>' . PHP_EOL;
 }
 include 'boards_common.php';
-include 'sql_param.php';
+include 'sql_reader.php';
 $conn = connect_sql($host, $user, $pass, $schema);
 $size = array_key_exists('board_size', $_GET) ? $_GET['board_size'] : 'm';
 $orb_count = array_key_exists('orb_count', $_GET) ? intval($_GET['orb_count']) : 2;
@@ -58,5 +72,13 @@ $boards = select_boards($conn, $size, $orb_count, $hearts);
 <?php 
 echo '<div class="boards float size-' . $size . '">' . display_boards($boards) . '</div>';
 ?>
+</section>
+    <footer>
+        <p class="title">PAD Boards Database</p>
+        <p class="bm"><a href="../">@ Blogging Mama</a></p>
+        <span class="credits heading">Creator</span><span class="credits">chu2</span><br />
+        <span class="credits heading">Data input</span><span class="credits">Umby, InsanityBringer</span>
+        <p>&copy; 2019 chu2. Puzzle & Dragons logo and all related images are registered trademarks or trademarks of GungHo Online Entertainment, Inc.</p>
+    </footer>
 </body>
 </html>
---
+++ sql_reader.php
@@ -0,0 +1,4 @@
+<?php
+//$host='pad.protic.site:3306';$user='proticsi_reader';$pass='ReadOnly';$schema='proticsi_optimal';
+$host='localhost';$user='root';$pass='admin';$schema='optimal';
+?>
