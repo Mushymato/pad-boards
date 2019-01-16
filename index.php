b005e30d5073e4808763400b1e32053af364c1c1
renimama <36003890+renimama@users.noreply.github.com>
2019-01-16 23:54:27 +0900

    Mobile support with Bootstrap.

--- index.php
+++ index.php
@@ -2,18 +2,26 @@
 <html>
 	<head>
         <meta charset='utf-8'/>
-        <title>PAD Boards Database @ Blogging Mama</title>
-        <link rel="stylesheet" type="text/css" href="boards.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>PAD Boards Database | Blogging Mama</title>
+        <link rel="stylesheet" type="text/css" href="boards2.css">
         <link rel="stylesheet" type="text/css" href="style.css">
         
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+        
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" type="text/javascript"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
         <script src="board_filters.js" type="text/javascript"></script>
+        
         <link rel="stylesheet" href="./colorbox/colorbox.css">
         <script src="./colorbox/jquery.colorbox.js"></script>
 
 		<script>
 			$(document).ready(function(){
-				$(".board-url").colorbox({iframe:true, width:"80%", height:"90%"});
+				$(".board-url").colorbox({iframe:true, width:"80%", height:"90%"});	
+				$(".how-to").colorbox({inline:true, width:"80%"});
+				$(".about").colorbox({inline:true, width:"80%"});
 			});
 		</script>
         <script>
@@ -29,19 +37,58 @@
 	</head>
 <body>
 
-    <div class="header">
-        <div class="header-bg">
-        </div>
-        <div class="content">
-            <span class="pad-title">Puzzle & Dragons</span>
-            <p class="heading-title"><a href="./">Boards Database</a></p>
-            <ul class="navbar">
-            	<li><a href="#">About</a></li>
-                <li><a href="../">Return to Blog</a></li>
-            </ul>
-        </div>
-    </div>
+    <nav style="background-color: #117c7c;" class="navbar navbar-expand-lg navbar-dark bg-dark">
     
+       <h5 class="my-0 mr-md-auto text-light site-name font-weight-bold"><a class="navbar-brand" href="../boards">PAD Boards Database</a></h5>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link how-to" href="#how-to">How to use</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link about" href="#about">Credits</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../">Blogging Mama</a>
+          </li>
+        </ul>
+        <span class="navbar-text">
+          A boards database created by chu2.
+        </span>
+      </div>
+    </nav>
+
+		<!-- How to use -->
+		<div style='display:none'>
+			<div id='how-to' style='padding:10px; background:#fff;'>
+    			<h2>PAD Boards Database</h2>
+        		<p>Optimal boards for all your needs!</p>
+                
+    			<h2>How to use</h2>
+                <ol>
+	                <li>Select a board size.</li>
+                    <li>Enter number of orbs.</li>
+                    <li>Optional: Select style of matching needed.</li>
+                    <li>Enjoy the board!</li>
+                </ol>
+                
+                <h2>Tips</h2>
+                <p>???</p>
+                
+			</div>
+		</div>
+        <!-- About -->
+		<div style='display:none'>
+			<div id='about' style='padding:10px; background:#fff;'>
+		    	<h2>Credits</h2>
+        		<span class="">Creator:</span> chu2<br>
+        		<span class="">Data entry & verification:</span> Umby, InsanityBringer, <a href="https://misopad.wordpress.com/" target="_blank">Miso#5924</a>
+			</div>
+		</div>
+<main role="main">
     <section id="display-boards">
         <?php
         function option($value, $text, $check){
@@ -54,37 +101,54 @@
         $orb_count = array_key_exists('orb_count', $_GET) ? intval($_GET['orb_count']) : 2;
         $hearts = array_key_exists('hearts', $_GET) ? boolval($_GET['hearts']) : false;
         $boards = select_boards($conn, $size, $orb_count, $hearts);
-        ?>
-        <form action='' class="grid fset">
-            <fieldset>
-                <legend>Select Board Type</legend>
-                <select name="orb_count">
-                    <?php
-                        echo option(2, 'Bicolor', $orb_count);
-                        echo option(3, 'Tricolor', $orb_count);
-                    ?>
-                </select>
-                <select name="board_size">
-                    <?php
-                        echo option('s', '5x4', $size);
-                        echo option('m', '6x5', $size);
-                        echo option('l', '7x6', $size);
-                    ?>
-                </select>
-                <select name="hearts">
-                    <?php
-                        echo option('0', 'no hearts', $hearts);
-                        echo option('1', 'with hearts', $hearts);
-                    ?>
-                </select>
-                <button type="submit" value="Submit">Submit</button>
-            </fieldset>
-            <?php echo get_attribute_filters($boards);?>
-        </form>
-        <?php 
-        echo '<div class="boards float size-' . $size . '">' . display_boards($boards) . '</div>';
-        ?>
+		?>
+        <!-- Board type -->
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+				<form action=''>
+					   <fieldset class="border p-2">
+						<legend class="w-auto">Select Board Type</legend>
+						<select name="orb_count">
+							<?php
+								echo option(2, 'Bicolor', $orb_count);
+								echo option(3, 'Tricolor', $orb_count);
+							?>
+						</select>
+						<select name="board_size">
+							<?php
+								echo option('s', '5x4', $size);
+								echo option('m', '6x5', $size);
+								echo option('l', '7x6', $size);
+							?>
+						</select>
+						<select name="hearts">
+							<?php
+								echo option('0', 'no hearts', $hearts);
+								echo option('1', 'with hearts', $hearts);
+							?>
+						</select>
+						<button type="submit" value="Submit">Submit</button>
+					</fieldset>
+					</form>
+					</div>
+				<div class="col-md-8">    
+					<form action=''>        
+					<?php echo get_attribute_filters($boards);?>
+					</form>
+				</div>
+			</div>
+		</div>
+        <!-- Board display -->
+		<div class="container">
+			<div class="row">
+					<?php 
+					echo '<div class="text-center boards float size-' . $size . '">' . display_boards($boards) . '</div>';
+					?>
+			</div>
+		</div>
 	</section>
+    </main>
     <footer>
         <p class="title">PAD Boards Database</p>
         <p class="bm"><a href="../">@ Blogging Mama</a></p>
