b801c871547afa9e97afb80da97732c9d10a393c
renimama <36003890+renimama@users.noreply.github.com>
2019-01-18 00:51:56 +0900

    Added Bootstrap support

--- solve_boards.php
+++ solve_boards.php
@@ -1,8 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <link rel="stylesheet" type="text/css" href="boards.css">
 <link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" type="text/javascript"></script>
 <script src="board_filters.js" type="text/javascript"></script>
 <script>
@@ -14,13 +17,16 @@ $(document).ready(function(){
 </head>
 <body>
 
-<div class="header">
-	<div class="header-bg">
+    <div class="header">
+        <div class="header-bg">
+        </div>
+        <div class="content">
+            <p class="heading-title">Step-by-step matches</p>
+        </div>
     </div>
-	<div class="content">
-        <p class="heading-title">Step-by-step matches</p>
-	</div>
-</div>
+    
+<div class="container">
+<div class="row justify-content-center">
 <?php
 include 'boards_common.php';
 include 'sql_param.php';
@@ -29,5 +35,7 @@ $pattern = array_key_exists('pattern', $_GET) ? $_GET['pattern'] : '1';
 echo display_board_solve($conn, $pattern);
 ?>
 <div><a href="../boards/" >Back to PAD Board Database</a></div>
+</div>
+</div>
 </body>
 </html>
